bd0d98394797eafc12b02ca7157af21d5dc4dad3
Gnanakeethan Balasubramaniam <gnanakeethan@users.noreply.github.com>
2020-10-17 12:53:43 +0530

    Update app.js
    Fixing ViewBox

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get("/getMediumBlogs", async (request, response) => {
               width="${((limit==1)?width:2*width)+config.default.margin_left+config.card.spacing}" 
               version="1.2" 
               height="${ (((Math.round(limit/2))*height)+config.default.margin_top*2+config.card.spacing*(Math.floor(limit/2))) }"
-              viewBox="0 0 ${ (((Math.round(limit/2))*height)+config.default.margin_top*2+config.card.spacing*(Math.floor(limit/2))) } ${((limit==1)?width:2*width)+config.default.margin_left+config.card.spacing}">`;
+              viewBox="0 0 ${((limit==1)?width:2*width)+config.default.margin_left+config.card.spacing} ${ (((Math.round(limit/2))*height)+config.default.margin_top*2+config.card.spacing*(Math.floor(limit/2))) }">`;
     await asyncForEach(resultData, request.query, async (blog, index, settings) => {
       if (index >= limit) {
         return;
